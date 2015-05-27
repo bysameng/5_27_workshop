2d31067b11a458310431d857418cad524c42c9c5
Samuel Eng <sam-eng@icloud.com>
2015-05-27 15:21:27 -0400

    swapper

--- Assets/Scripts/Swapper.cs
+++ Assets/Scripts/Swapper.cs
@@ -7,8 +7,15 @@ public class Swapper : MonoBehaviour {
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.tag == "Swappable"){
 			print (other.gameObject.name);
+
+			this.gameObject.tag = "Swappable";
+			other.gameObject.tag = "Player";
+
 			other.gameObject.AddComponent<PlayerInput>();
+			other.gameObject.AddComponent<Swapper>();
+
 			DestroyPlayerInput();
+			Destroy(this);
 		}
 	}
 
