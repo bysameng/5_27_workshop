7dee9c9e5a5a920d0d6cc13eb17aa25b43e481fe
Samuel Eng <sam-eng@icloud.com>
2015-05-27 13:58:52 -0400

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private float maxSpeed;
+	private float currentSpeed;
+	private float acceleration;
+	private float deceleration;
+
+
+	// Use this for initialization
+	void Start () {
+		maxSpeed = 10f;
+		currentSpeed = 0f;
+		acceleration = 2f;
+		deceleration = 4f;
+	}
+
+
+	void Update(){
+		bool isUpArrowPressed = Input.GetKey(KeyCode.UpArrow);
+
+		if (isUpArrowPressed){
+			currentSpeed += acceleration * Time.deltaTime;
+			//currentSpeed = currentSpeed + acceleration * Time.deltaTime;
+		}
+		else{
+			currentSpeed -= deceleration * Time.deltaTime;
+			if (currentSpeed < 0){
+				currentSpeed = 0;
+			}
+		}
+
+		transform.position = transform.position + new Vector3(0f, 0f, currentSpeed) * Time.deltaTime;
+	}
+
+
+
+	
+
+}
---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerInput : MonoBehaviour {
+
+	private Rigidbody rbody;
+	private Renderer rend;
+
+	public float movementForce;
+
+	void Start () {
+		rbody = this.GetComponent<Rigidbody>();
+		rend = this.GetComponent<Renderer>();
+
+//		if (rbody == null){
+//			rbody = this.gameObject.AddComponent<Rigidbody>();
+//		}
+	}
+	
+	void Update () {
+		float h = Input.GetAxis("Horizontal") * movementForce;
+		float v = Input.GetAxis("Vertical") * movementForce;
+
+		print ("Horizontal: " + h + " Vertical: " + v);
+
+		Vector3 movement = new Vector3(h, 0f, v);
+		rbody.AddForce(movement);
+
+
+		if (Input.GetKeyDown(KeyCode.F)){
+			ChangeColor();
+		}
+
+	}
+
+
+	void ChangeColor(){
+		rend.material.color = Color.red;
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
